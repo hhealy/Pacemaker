46e0a088e05440227d44bf5771d3b9f13216fc6e
Henry Healy <henryjdhealy@gmail.com>
2016-10-02 02:59:18 +0100

    PacemakerAPITest, Static Fixtures and other tests.

--- src/models/User.java
+++ src/models/User.java
@@ -38,6 +38,23 @@ public class User
 	  }
 	  
 	  @Override
+	  public boolean equals(final Object obj)
+	  {
+		  if (obj instanceof User)
+		  {
+			  final User other = (User) obj;
+		      return Objects.equal(firstName, other.firstName) 
+		          && Objects.equal(lastName,  other.lastName)
+		          && Objects.equal(email,     other.email)
+		          && Objects.equal(password,  other.password);
+		  }
+		  else
+		  {
+			  return false;
+		  }
+	  }
+	  
+	  @Override
 	  public String toString()
 	  {
 	    return toStringHelper(this).addValue(id)
